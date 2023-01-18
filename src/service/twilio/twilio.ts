b21c5c9123990375c5754d03a528ad32ea6b26ec
Softworth Solutions Private Limited <79741172+harikrishnan-sws@users.noreply.github.com>
2023-01-19 01:57:24 +0530

    added service for purchasing list of available phone numbers

--- src/service/twilio/twilio.ts
+++ src/service/twilio/twilio.ts
@@ -1,5 +1,5 @@
 import http from "../../axios";
-import { PhoneNumberResult } from "./types";
+import { PhoneNumberResult, PurchaseResponse } from "./types";
 
 export const getPhoneNumberList = async (
   areaCode: number,
@@ -11,6 +11,22 @@ export const getPhoneNumberList = async (
     auth: {
       username: sid!,
       password: process.env.REACT_APP_TWILIO_AUTH_TOKEN_FE!,
+    },
+  });
+};
+
+export const purchaseAvailablePhoneNumber = async (phoneNumber: string) => {
+  const sid = process.env.REACT_APP_TWILIO_SID_FE;
+  const apiUrl = `${process.env.REACT_APP_TWILIO_API_URL}/2010-04-01/Accounts/${sid}/IncomingPhoneNumbers.json`;
+  const params = new URLSearchParams();
+  params.append("PhoneNumber", phoneNumber);
+  return http.post<PurchaseResponse>(apiUrl, params, {
+    auth: {
+      username: sid!,
+      password: process.env.REACT_APP_TWILIO_AUTH_TOKEN_FE!,
+    },
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
     },
   });
 };
--- src/service/twilio/types.ts
+++ src/service/twilio/types.ts
@@ -1,27 +1,70 @@
-export type PhoneNumberResult = {
-    available_phone_numbers: TwilioPhoneNumber[],
-    uri: string;
-}
-
+// region common
 export type Capabilities = {
-    voice: boolean;
-    SMS: boolean;
-    MMS: boolean;
-}
+  voice: boolean;
+  SMS: boolean;
+  MMS: boolean;
+};
+// end region
+
+// region getPhoneNumberList
+export type PhoneNumberResult = {
+  available_phone_numbers: TwilioPhoneNumber[];
+  uri: string;
+};
 
 export type TwilioPhoneNumber = {
-    friendly_name: string;
-    phone_number: string;
-    lata: string;
-    rate_center: string;
-    latitude: string;
-    longitude: string;
-    locality: string;
-    region: string;
-    postal_code: string;
-    iso_country: string;
-    address_requirements: string;
-    beta: boolean;
-    capabilities: Capabilities
-}
+  friendly_name: string;
+  phone_number: string;
+  lata: string;
+  rate_center: string;
+  latitude: string;
+  longitude: string;
+  locality: string;
+  region: string;
+  postal_code: string;
+  iso_country: string;
+  address_requirements: string;
+  beta: boolean;
+  capabilities: Capabilities;
+};
+// end region
 
+// region purchaseAvailablePhoneNumber
+
+export type PurchaseResponse = {
+  sid: string;
+  account_sid: string;
+  friendly_name: string;
+  phone_number: string;
+  voice_url: string;
+  voice_method: string;
+  voice_fallback_url: string;
+  voice_fallback_method: string;
+  voice_caller_id_lookup: boolean;
+  date_created: string;
+  date_updated: string;
+  sms_url: string;
+  sms_method: string;
+  sms_fallback_url: string;
+  sms_fallback_method: string;
+  address_requirements: string;
+  beta: boolean;
+  capabilities: Capabilities;
+  status_callback: string;
+  status_callback_method: string;
+  api_version: string;
+  voice_application_sid: string;
+  sms_application_sid: string;
+  origin: string;
+  trunk_sid: string;
+  emergency_status: string;
+  emergency_address_sid: string;
+  emergency_address_status: string;
+  address_sid: string;
+  identity_sid: string;
+  bundle_sid: string;
+  uri: string;
+  status: string;
+};
+
+// end region
